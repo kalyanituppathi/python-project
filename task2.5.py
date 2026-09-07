be491344450baa8f3c295cdcc1afde7d2c941742
kalyani <kalyanituppathi5@gmail.com>
2026-09-07 23:37:21 +0530

    First commit

---
+++ task2.5.py
@@ -0,0 +1,21 @@
+#Task 5: Modules & Packages
+import math
+import random
+import datetime
+number=int(input("enter a number:"))
+sqrt_val=math.sqrt(number)
+print(f"Suareroot of {number} if {sqrt_val}")
+random_no=random.randint(1,100)#generates a random integer between 1 and 100
+print(f"random number between 1 and 100 :{random_no}")
+cur_date=datetime.datetime.now()
+print(f"Current date and time :{cur_date}")
+#Explanation
+#math Module :it provides functions like square root ,trignometry functions,logarithms factorial ,etc.
+#random Module
+#Used for generating random numbers, shuffling sequences, or selecting random elements.
+#Example used: random.randint(1, 100) → returns a random integer between 1 and 100.
+#datetime Module
+#Helps in working with dates and times, including current time, formatting, and arithmetic with dates.
+#Example used: datetime.datetime.now() → returns the curandom Module
+#Used for generating random numbers, shuffling sequences, or selecting random elements.
+#Example used: random.randint(1, 100) → returns a random integer between 1 and 100.
