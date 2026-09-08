95ad6a3439e3b4e6d774689b4961c14a56d83b2d
kalyani <kalyanituppathi5@gmail.com>
2026-09-08 21:52:04 +0530

    Added README and organized folders

---
+++ src/task1.py
@@ -0,0 +1,4 @@
+#task 1:simple print
+print("Hello,World!")
+#print():its a function which prints or gives output
+#"Hello,World!":its a text or string which must consists of double quotes to get output
