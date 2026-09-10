0c8ab67365432e703077c5ad37afd50f63476329
kalyani <kalyanituppathi5@gmail.com>
2026-09-10 19:44:00 +0530

    Updated folder structure

--- src/task1.py
+++
@@ -1,4 +0,0 @@
-#task 1:simple print
-print("Hello,World!")
-#print():its a function which prints or gives output
-#"Hello,World!":its a text or string which must consists of double quotes to get output
